50c2d83df03490736b1772f5e9b3379990bad769
Artem <a.bobrov@remont.io>
2023-12-07 14:12:11 +0300

    update

--- src/components/SwipeableTextMobileStepper/SwipeableTextMobileStepper.tsx
+++ src/components/SwipeableTextMobileStepper/SwipeableTextMobileStepper.tsx
@@ -15,44 +15,44 @@ const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 const images = [
   {
-    label: 'San Francisco – Oakland Bay Bridge',
+    // label: 'San Francisco – Oakland Bay Bridge',
     imgPath:
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+      '/1.jpg',
   },
   {
-    label: 'Bird',
+    // label: 'Bird',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+      '/2.jpg',
   },
   {
-    label: 'Bali, Indonesia',
+    // label: 'Bali, Indonesia',
     imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+      '/3.jpg',
   },
   {
-    label: 'Goč, Serbia',
+    // label: 'Goč, Serbia',
     imgPath:
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+      '/4.jpg',
   },
   {
-    label: 'Bird',
+    // label: 'Bird',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+      '/5.jpg',
   },
   {
-    label: 'Bali, Indonesia',
+    // label: 'Bali, Indonesia',
     imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+      '/6.jpg',
   },
   {
-    label: 'Bird',
+    // label: 'Bird',
     imgPath:
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+      '/7.jpg',
   },
   {
-    label: 'Bali, Indonesia',
+    // label: 'Bali, Indonesia',
     imgPath:
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+      '/8.jpg',
   },
 ];
 
@@ -92,7 +92,7 @@ function SwipeableTextMobileStepper() {
         
         }}
       >
-        <Typography>{images[activeStep].label}</Typography>
+        {/* <Typography>{images[activeStep].label}</Typography> */}
       </Paper>
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
